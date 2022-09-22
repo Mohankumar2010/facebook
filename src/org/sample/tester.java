3a802ff4cfaa4f30d13d566d80334896d6359aba
Mohankumar <mohanmog95@gmail.com>
2022-09-22 19:05:54 +0530

    message

---
+++ src/org/sample/tester.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class tester {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\eclipse-workspace\\FaceBook\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+	}
+}
